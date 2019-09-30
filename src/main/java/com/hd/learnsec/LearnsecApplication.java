ee308d1b01512e84a4b880bb6584cfb52c5a9849
Puck <825597202@qq.com>
2019-09-30 22:40:24 +0800

    start 9.30

---
+++ src/main/java/com/hd/learnsec/LearnsecApplication.java
@@ -0,0 +1,13 @@
+package com.hd.learnsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnsecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnsecApplication.class, args);
+	}
+
+}
