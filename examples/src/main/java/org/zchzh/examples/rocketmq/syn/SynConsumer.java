348991210ac18867594ed052409137df86d4d346
zengchzh <825597202@qq.com>
2021-06-23 16:44:28 +0800

    添加rocketmq同步消息使用例子

---
+++ examples/src/main/java/org/zchzh/examples/rocketmq/syn/SynConsumer.java
@@ -0,0 +1,33 @@
+package org.zchzh.examples.rocketmq.syn;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author zengchzh
+ * @date 2021/6/23
+ */
+public class SynConsumer {
+
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("test_syn");
+        consumer.setNamesrvAddr("localhost:9876");
+        consumer.subscribe("TopicTest", "*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
---
+++ examples/src/main/java/org/zchzh/examples/rocketmq/syn/SynProducer.java
@@ -0,0 +1,35 @@
+package org.zchzh.examples.rocketmq.syn;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+/**
+ * @author zengchzh
+ * @date 2021/6/23
+ * 同步消息发送
+ */
+public class SynProducer {
+
+    public static void main(String[] args) throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException, MQBrokerException {
+        DefaultMQProducer producer = new DefaultMQProducer("test_syn");
+        producer.setNamesrvAddr("localhost:9876");
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            Message msg = new Message("TopicTest",
+                    "TagA",
+                    ("Hello RocketMQ" + i).getBytes(RemotingHelper.DEFAULT_CHARSET)
+            );
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        producer.shutdown();
+    }
+}
