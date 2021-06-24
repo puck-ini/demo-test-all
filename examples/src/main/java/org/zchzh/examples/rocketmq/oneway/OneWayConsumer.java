e986b0f31d6a432eaa3aea2c3ee08aabc9c5f961
zengchzh <825597202@qq.com>
2021-06-24 09:37:14 +0800

    添加rocketmq单向发送消息使用例子

---
+++ examples/src/main/java/org/zchzh/examples/rocketmq/oneway/OneWayConsumer.java
@@ -0,0 +1,32 @@
+package org.zchzh.examples.rocketmq.oneway;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @author zengchzh
+ * @date 2021/6/24
+ */
+public class OneWayConsumer {
+
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("test_one_way");
+        consumer.setNamesrvAddr("localhost:9876");
+        consumer.subscribe("TopicOneWay", "*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
---
+++ examples/src/main/java/org/zchzh/examples/rocketmq/oneway/OneWayProducer.java
@@ -0,0 +1,33 @@
+package org.zchzh.examples.rocketmq.oneway;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author zengchzh
+ * @date 2021/6/23
+ */
+public class OneWayProducer {
+    public static void main(String[] args) throws MQClientException, RemotingException, InterruptedException, UnsupportedEncodingException {
+        DefaultMQProducer producer = new DefaultMQProducer("test_one_way");
+        producer.setNamesrvAddr("localhost:9876");
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            // 创建消息，并指定Topic，Tag和消息体
+            Message msg = new Message("TopicOneWay" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            // 发送单向消息，没有任何返回结果
+            producer.sendOneway(msg);
+        }
+        // 如果不再发送消息，关闭Producer实例。
+        producer.shutdown();
+
+    }
+}
