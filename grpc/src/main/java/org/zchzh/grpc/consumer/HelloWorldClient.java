acd09310238faef9f4679a0f0619953dc4281cad
zengchzh <825597202@qq.com>
2021-08-17 15:55:30 +0800

    feat(grpc):客户端添加异步请求以及future请求方式

--- grpc/src/main/java/org/zchzh/grpc/consumer/HelloWorldClient.java
+++ grpc/src/main/java/org/zchzh/grpc/consumer/HelloWorldClient.java
@@ -1,10 +1,14 @@
 package org.zchzh.grpc.consumer;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 import org.zchzh.grpc.api.GreeterGrpc;
 import org.zchzh.grpc.api.HelloWorld;
+
+import java.util.concurrent.*;
 
 /**
  * @author zengchzh
@@ -32,6 +36,10 @@ public class HelloWorldClient {
         String user;
         user = "world0";
         client.doSync(user);
+        user = "world1";
+        client.doAsync(user);
+        user = "world2";
+        client.doFuture(user);
     }
 
 
@@ -41,17 +49,64 @@ public class HelloWorldClient {
         HelloWorld.HelloReply reply;
         try {
             reply = blockingStub.sayHello(request);
+            System.out.println(reply);
         } catch (StatusRuntimeException e) {
             e.printStackTrace();
         }
     }
 
     public void doAsync(String name) {
-//        GreeterGrpc.GreeterStub stub = GreeterGrpc.GreeterStub.newStub(getChannel());
+        GreeterGrpc.GreeterStub stub = GreeterGrpc.newStub(getChannel());
+        HelloWorld.HelloRequest request = HelloWorld.HelloRequest.newBuilder().setName(name).build();
+        stub.sayHello(request, new StreamObserver<HelloWorld.HelloReply>() {
+            @Override
+            public void onNext(HelloWorld.HelloReply helloReply) {
+                System.out.println(helloReply);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("completed");
+            }
+        });
 
     }
 
     public void doFuture(String name) {
-//        GreeterGrpc.GreeterFutureStub futureStub = GreeterGrpc.GreeterFutureStub.newStub(getChannel());
+        GreeterGrpc.GreeterFutureStub futureStub = GreeterGrpc.newFutureStub(getChannel());
+        HelloWorld.HelloRequest request = HelloWorld.HelloRequest.newBuilder().setName(name).build();
+        ListenableFuture<HelloWorld.HelloReply> future = futureStub.sayHello(request);
+        future.addListener(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("listener");
+            }
+        }, new ThreadPoolExecutor(12,
+                24,
+                3000,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(10),
+                new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setDaemon(true);
+                thread.setName("test-grpc-" + r.hashCode());
+                return thread;
+            }
+        }));
+
+        try {
+            HelloWorld.HelloReply reply = future.get();
+            System.out.println(reply);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 }
